84b88469a65b19f8dbe183fd731d62f1701c0fc3
Alex D. Raj <mr.alexdraj@gmail.com>
2026-04-21 14:51:58 +0530

    Update Processing's lab selection.py

--- pages/Processing's lab selection.py
+++ pages/Processing's lab selection.py
@@ -79,7 +79,7 @@ col1, col2 = st.columns(2)
 
 with col1:
     if st.button("Exhaustion Percentage", use_container_width=True):
-        st.switch_page("pages/Exhaustion Percentage.py")
+        st.switch_page("pages/Exhaustion percentage.py")
 
 with col2:
     if st.button("Fixation Percentage", use_container_width=True):
